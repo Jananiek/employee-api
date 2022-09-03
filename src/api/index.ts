5158b9da0aca61c6f3d8646c1e5531bbf12cbe98
jananie-wathsala <jananie.kum@gmail.com>
2022-09-03 12:02:58 +0530

    add api for create employee

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import employee from './routes/employee';
 import health from './routes/health';
 
 export default () => {
   const app = Router();
   health(app);
+  employee(app);
 
   return app;
 };
